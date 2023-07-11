79617ac74c33198d883065178cd7213c72c530c8
chernnovaa <katac0747@gmail.com>
2023-07-11 23:49:25 +0300

    Added a new section

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const menuBtn = document.querySelector(".menu__btn");
+const menuMob = document.querySelector(".menu__header-list");
+
+menuBtn.addEventListener("click", () => {
+  menuMob.classList.toggle("menu--open");
+});
