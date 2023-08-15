c32f85729e37413d929cb5717b11d7b49e86b6eb
chernnovaa <katac0747@gmail.com>
2023-08-15 22:10:44 +0300

    Added section slider

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@ const menuMob = document.querySelector(".menu__header-list");
 menuBtn.addEventListener("click", () => {
   menuMob.classList.toggle("menu--open");
 });
+
+const swiperOne = new Swiper(".feedback-slider", {
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
