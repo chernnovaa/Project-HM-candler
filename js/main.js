dbfc4e7f703ca9af83a3a4126c27720d81f115f0
chernnovaa <katac0747@gmail.com>
2023-09-29 14:04:00 +0300

    Added section questions

--- js/main.js
+++ js/main.js
@@ -22,3 +22,22 @@ const swiperTwo = new Swiper(".certificates__slider", {
     el: ".swiper-pagination",
   },
 });
+
+const accordeon = document.querySelector(".accordeon");
+const accordeonTitles = document.querySelectorAll(".accordeon__title");
+
+accordeonTitles.forEach.call(accordeonTitles, function (e) {
+  e.addEventListener("click", function () {
+    const curText = e.parentElement.querySelector(".accordeon__text");
+
+    e.classList.toggle("accordeon__title--active");
+
+    curText.classList.toggle("accordeon__text--visible");
+
+    if (curText.classList.contains("accordeon__text--visible")) {
+      curText.style.maxHeight = curText.scrollHeight + "px";
+    } else {
+      curText.style.maxHeight = null;
+    }
+  });
+});
